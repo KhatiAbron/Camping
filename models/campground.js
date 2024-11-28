433702f3ee5b760343feb1b985f9f0ecff43403f
Khatereh Abron <khatereh.abron@gmail.com>
2024-11-28 13:56:23 +0100

    Updated project files

--- models/campground.js
+++ models/campground.js
@@ -3,20 +3,9 @@ const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    image: String,
-    price: Number,
+    price: String,
     description: String,
-    location: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-    },
-    reviews: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Review'
-        }
-    ]
+    location: String
 });
 
-module.exports = mongoose.model('Campground', CampgroundSchema)
+module.exports = mongoose.model('Campground', CampgroundSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -6,11 +6,12 @@ const Campground = require('../models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    //useCreateIndex: true,
+    //useCreateIndex: false,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -23,14 +24,12 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 20) + 10;
+
         const camp = new Campground({
 
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
-            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint maxime eligendi sequi voluptate necessitatibus provident natus officia, pariatur facere iusto repudiandae porro nesciunt at suscipit debitis? Optio recusandae distinctio maxime!',
-            price
+
         })
         await camp.save();
     }
