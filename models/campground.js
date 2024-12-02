b32c27656691c60240846ca5b94b8da1e01a6d52
Khatereh Abron <khatereh.abron@gmail.com>
2024-12-02 14:14:55 +0100

    add images, navbar and footer

--- models/campground.js
+++ models/campground.js
@@ -1,9 +1,11 @@
+const { string } = require('joi');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title: String,
-    price: String,
+    image: String,
+    price: Number,
     description: String,
     location: String
 });
