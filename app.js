a61a1cf0f9522f56d33d86020ba356518802c34b
Khatereh Abron <khatereh.abron@gmail.com>
2024-12-02 11:52:57 +0100

    Fix: Ignore node_modules directory

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-//const ejsMate = require('ejs-mate');
+const ejsMate = require('ejs-mate');
 //const session = require('express-session');
 //const flash = require('connect-flash');
 //const ExpressError = require('./utils/ExpressError');
@@ -30,7 +30,7 @@ db.once("open", () => {
 
 const app = express();
 
-//app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
