4428a2cbe8a48ea3c2f0c77758f1f30fa63026ae
Khatereh Abron <khatereh.abron@gmail.com>
2024-12-07 08:11:11 +0100

    Catching Errors

--- app.js
+++ app.js
@@ -2,19 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-//const session = require('express-session');
-//const flash = require('connect-flash');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
-//const passport = require('passport');
-//const LocalStrategy = require('passport-local');
-//const User = require('./models/user');
 const Campground = require('./models/campground');
 
-//const userRoutes = require('./routes/users');
-//const campgroundRoutes = require('./routes/campgrounds');
-//const reviewRoutes = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -37,38 +29,7 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-//app.use(express.static(path.join(__dirname, 'public')))
 
-/*const sessionConfig = {
-    secret: 'thisshouldbeabettersecret!',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-        httpOnly: true,
-        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
-        maxAge: 1000 * 60 * 60 * 24 * 7
-    }
-}*/
-
-//app.use(session(sessionConfig))
-//app.use(flash());
-
-//app.use(passport.initialize());
-//app.use(passport.session());
-//passport.use(new LocalStrategy(User.authenticate()));
-
-//passport.serializeUser(User.serializeUser());
-//passport.deserializeUser(User.deserializeUser());
-
-/*app.use((req, res, next) => {
-    res.locals.success = req.flash('success');
-    res.locals.error = req.flash('error');
-    next();
-})*/
-
-//app.use('/', userRoutes);
-//app.use('/campgrounds', campgroundRoutes)
-//app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 app.get('/', (req, res) => {
     res.render('Home')
@@ -83,13 +44,12 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
-app.post('/campgrounds', catchAsync(async (req, res, next) => {
-
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-
-}));
+}))
 
 
 app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
@@ -102,9 +62,7 @@ app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }));
 
-/*app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})*/
+
 
 app.put('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -114,11 +72,7 @@ app.put('/campgrounds/:id', catchAsync(async (req, res) => {
 
 
 
-/*app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
-})*/
+
 
 app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -126,10 +80,17 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
-app.use((err, req, res, next) => {
-    res.send("oh! something went wrong")
 
-});
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
