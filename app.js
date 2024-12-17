4841844b28cee0e248b41f9892acf7e3c1b3d8db
Khatereh Abron <khatereh.abron@gmail.com>
2024-12-17 10:36:26 +0100

    Added register route and form

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+
+
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -69,11 +73,10 @@ app.use((req, res, next) => {
 })
 
 
-app.get
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
 
 app.get('/', (req, res) => {
     res.render('Home')
