ba52ed36aa4b6e9258ebe49e2b1816cc53044f2c
Khatereh Abron <khatereh.abron@gmail.com>
2024-12-21 22:22:43 +0100

    Added logout

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
@@ -24,5 +25,26 @@ router.post('/register', catchAsync(async (req, res, next) => {
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
 
 module.exports = router;
