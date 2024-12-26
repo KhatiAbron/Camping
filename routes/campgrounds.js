649e68887b02199400d119c359dcac54ff3fff2a
Khatereh Abron <khatereh.abron@gmail.com>
2024-12-26 17:57:31 +0100

    Multer middleware

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const campgrounds = require('../controllers/campgrounds');
-
 const catchAsync = require('../utils/catchAsync');
-//const { campgroundSchema } = require('../schemas.js');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads' });
 
-//const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
+
 
 
 
@@ -15,7 +15,12 @@ const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    //.post(isLoggedIn, upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.single('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("It Worked");
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
